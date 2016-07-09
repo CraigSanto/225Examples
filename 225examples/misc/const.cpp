967e18a4c423206ade8affe345b2be569eeecfea
CraigSanto <craigsanto97@gmail.com>
2016-07-09 17:06:42 -0500

    Adding misc directory and const example

---
+++ 225examples/misc/const.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+//brief showcase of const
+
+//test1: If we don't use const, our compiler will allow this
+//Is what we do here wrong? Not inherently. It depends on our goal.
+void test1(){
+	int x = 7;
+	int &p = x;
+	p = 14;
+	cout << x << endl;
+}
+
+//test2: The same as test2, but now int &p is const
+//Uncomment test2 to see the compiler throw an error
+//"Read-only variable is not assignable"
+/*void test2(){
+	int x = 7;
+	const int &p = x;
+	p = 14;
+	cout << x << endl;
+}*/
+
+//What if we modify x instead of p?
+//We see that we can sneak around "read-only" items with pointers!
+//Use this power carefully!
+void test3(){
+	int x = 7;
+	const int &p = x;
+	x = 14;
+	cout << x << endl;
+	cout << p << endl;
+}
+
+int main(){
+	test1();
+	//test2();
+	test3();
+
+	return 0;
+}
